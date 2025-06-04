fd2f2169978914f301e6c9176aa5ec578b1d1893
thangarajan1 <saaralk.t.rajan2@gmail.com>
2025-06-04 18:12:46 +0530

    commit

--- src/page/main/tlModal/tlModal.js
+++ src/page/main/tlModal/tlModal.js
@@ -3,14 +3,16 @@ import "./index.scss";
 import React from "react";
 
 const TlModal = ({ open, onOk, onCancel, children, ...resetProps }) => {
-    return <Modal
-        open={open}
-        onOk={onOk}
-        onCancel={onCancel}
-        {...resetProps}
-    >
-        {children}
-    </Modal>
+    return (
+        <Modal
+            open={open}
+            onOk={onOk}
+            onCancel={onCancel}
+            {...resetProps}
+        >
+            {children}
+        </Modal>
+    );
 };
 
 export default TlModal;
