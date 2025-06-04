3729f5fdd238e3a2cadd86fbfff61712aceadc08
thangarajan1 <saaralk.t.rajan2@gmail.com>
2025-06-04 12:51:01 +0530

    commit changes

---
+++ dist/assets/images/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "DownArrow", {
+  enumerable: true,
+  get: function get() {
+    return _downArrow.DownArrow;
+  }
+});
+var _downArrow = require("./down-arrow.svg");
--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,12 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+Object.defineProperty(exports, "TlAccordian", {
+  enumerable: true,
+  get: function get() {
+    return _tlAccordian["default"];
+  }
+});
 Object.defineProperty(exports, "UiButton", {
   enumerable: true,
   get: function get() {
@@ -10,4 +16,5 @@ Object.defineProperty(exports, "UiButton", {
   }
 });
 var _UiButton = _interopRequireDefault(require("./UiButton"));
+var _tlAccordian = _interopRequireDefault(require("./tlAccordian"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
