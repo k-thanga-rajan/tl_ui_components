f2839b3f618618e490df2c6ccc9fa562692de60f
thangarajan1 <saaralk.t.rajan2@gmail.com>
2025-06-04 11:56:02 +0530

    commit

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "UiButton", {
+  enumerable: true,
+  get: function get() {
+    return _UiButton["default"];
+  }
+});
+var _UiButton = _interopRequireDefault(require("./UiButton"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
--- index.js
+++
@@ -1,3 +0,0 @@
-import UiButton from "./src/UiButton";
-
-export { UiButton };
