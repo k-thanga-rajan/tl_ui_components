97b0d55699d8afe65f418f1497fd794d66f6e0fa
thangarajan1 <saaralk.t.rajan2@gmail.com>
2025-06-04 18:03:41 +0530

    commit

--- src/page/main/tlModal/tlModal.js
+++ src/page/main/tlModal/tlModal.js
@@ -1,6 +1,7 @@
 import { Modal } from "antd";
 import "./index.scss";
 import React from "react";
+
 const TlModal = ({ open, onOk, onCancel, children, ...resetProps }) => {
     return (
         <>
