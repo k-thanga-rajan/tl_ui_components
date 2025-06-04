7014a28352d0df7596c0a615f05fc77671a4d76d
thangarajan1 <saaralk.t.rajan2@gmail.com>
2025-06-04 20:05:21 +0530

    commit changes

--- src/index.js
+++ src/index.js
@@ -7,3 +7,4 @@ export { default as TlSelect } from './page/main/TlSelect/tlSelect';
 export { default as TlTab } from './page/main/TlTab/tlTab';
 export { default as TlModal } from './page/main/tlModal/tlModal';
 export { default as TlShowConfirmModal } from './page/main/tlModal/tlShowConfirmModal';
+export { default as TlSearch } from './page/main/tlSearch/tlSearch';
---
+++ src/page/main/tlSearch/tlSearch.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Search from 'antd/es/input/Search';
+import "./index.scss";
+
+const TlSearch = React?.forwardRef(({ className, style, placeholder, handleSearch, searchValue, ...restProps }, ref) => (
+    <Search
+        ref={ref}
+        className={`custom-ant-search ${className}`}
+        style={style}
+        placeholder={placeholder ? placeholder : "Search"}
+        onSearch={handleSearch}
+        onChange={(e) => handleSearch(e.target.value)}
+        value={searchValue}
+        {...restProps}
+    />
+
+));
+export default TlSearch;
